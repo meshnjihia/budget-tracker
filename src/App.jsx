41be2ebe768823b01a74f413e05ba83248c0f2b8
meshnjihia <meshworks7@gmail.com>
2023-05-24 09:03:21 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
       <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)] ">
         <Routes>
           <Route path="/create-budget" element={<CreateBudget />} />
-          <Route path="/" element={<Home />} />
+          {/* <Route path="/" element={<Home />} /> */}
         </Routes>
       </main>
     </BrowserRouter>
